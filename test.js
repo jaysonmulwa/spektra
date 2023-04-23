b12609d507199ddce928ecc763f1f907a3d3a5b1
Jayson Mulwa <jayson.mulwa@gmail.com>
2023-04-23 17:24:57 +0300

    update

--- test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var determinant_1 = require("./src/determinant");
-var mean_1 = require("./src/mean");
-var eigenvalues_1 = require("./src/eigenvalues");
-var matrix = [[4, 1], [4, 1]];
-console.log((0, determinant_1.determinant)(matrix), (0, mean_1.mean)(matrix), (0, eigenvalues_1.eigenvalues)(matrix));
